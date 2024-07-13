090573da9c493ff890d822f3225353009d3e2ede
facinick <facinick@gmail.com>
2024-07-13 17:50:11 +0530

    logger module

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,54 @@
+import { Logger, Injectable, Scope } from '@nestjs/common';
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class AppLogger extends Logger {
+
+    setContext = (context: string) => {
+        this.context = context;
+    }
+
+    log = (message: string, data?: any) => {
+        const timestamp = Date.now()
+        if(data) {
+            super.log(`[${timestamp}] ${message}`, data);
+        } else {
+            super.log(`[${timestamp}] ${message}`);
+        }
+    }
+
+    error = (message: string, data?: string) => {
+        const timestamp = Date.now()
+        if(data) {
+            super.error(`[${timestamp}] ${message}`, data);
+        } else {
+            super.error(`[${timestamp}] ${message}`);
+        }
+    }
+
+    warn = (message: string, data?: any) => {
+        const timestamp = Date.now()
+        if(data) {
+            super.warn(`[${timestamp}] ${message}`, data);
+        } else {
+            super.warn(`[${timestamp}] ${message}`);
+        }
+    }
+
+    debug = (message: string, data?: any) => {
+        const timestamp = Date.now()
+        if(data) {
+            super.debug(`[${timestamp}] ${message}`, data);
+        } else {
+            super.debug(`[${timestamp}] ${message}`);
+        }
+    }
+
+    verbose = (message: string, data?: any) => {
+        const timestamp = Date.now()
+        if(data) {
+            super.verbose(`[${timestamp}] ${message}`, data);
+        } else {
+            super.verbose(`[${timestamp}] ${message}`);
+        }
+    }
+}
